06808fffabfc8dc4c505e139ae79d9d0180b1bcb
Ahmad Kanounji <ahmadkanounji@gmail.com>
2023-03-11 18:40:01 +0200

    Version 0
    daouk is strong

---
+++ Kano/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Kano;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Kano/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Kano;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
